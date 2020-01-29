e869d51f6c7059d4700df05cbc6a58c3d8709cb7
Yul Jae Lee <yuljlee9@gmail.com>
2020-01-28 23:41:02 -0800

    feat: Add theme data

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Color(0xff00796B),
+        accentColor: Color(0xff7C4DFF),
+        textTheme: TextTheme(
+          body1: TextStyle(
+            fontSize: 24,
+            fontStyle: FontStyle.italic,
+          )
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Building Layouts with Flutter'),
           ),
         body: Center(
           child: Text('Hello Flutter Layouts',
-            style: TextStyle(fontSize: 24),
             ),
           ),
         floatingActionButton: FloatingActionButton(
